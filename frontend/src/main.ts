7cab668a557c30a306da074063a7af37b65b6906
Łukasz Tenerowicz <konkit@gmail.com>
2020-06-16 23:41:09 +0200

    Add Sentry

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,9 +5,20 @@ import store from './store';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
 Vue.config.productionTip = false;
 
 Vue.use(Buefy);
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://1761ba23418845ce91aeead31f914028@o245698.ingest.sentry.io/5278976',
+    integrations: [new VueIntegration({Vue, attachProps: true})],
+  });
+}
+
 
 new Vue({
   router,
